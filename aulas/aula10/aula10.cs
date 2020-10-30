ac9ad7bd87f1a37527dd8b8c69ff5b457ae40243
Aleleonel <aleleonel@gmail.com>
2020-10-30 17:39:02 -0300

    complementos typecast

---
+++ aulas/aula10/aula10.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Aula10 {
+    enum diassemana{Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado};
+
+    static void Main() {
+        diassemana ds = (diassemana)0;
+        Console.WriteLine(ds);
+
+    }
+}
---
+++ aulas/aula11/REDME.md
@@ -0,0 +1,1 @@
+conversão de tipos (typecast)
